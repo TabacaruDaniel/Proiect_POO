9ca45568c5b089a06ce0b9754f23739bbe751075
giuliamarin1011 <mm724@student.ugal.ro>
2025-11-19 19:06:02 +0200

    Create RezultatMotosapa.java

---
+++ RezultatMotosapa.java
@@ -0,0 +1,29 @@
+package poo1.aparat;
+
+public class RezultatMotosapa extends rezultat {
+
+    private double pamant_lucrat;
+    private double timp_lucru;
+
+    public RezultatMotosapa(double produs, double curent_consumat,
+                            float[] stare_perii, float volum_ramas,
+                            double functionare_ramasa,
+                            double pamant_lucrat, double timp_lucru) {
+
+        super(produs, curent_consumat, stare_perii, volum_ramas, functionare_ramasa);
+        this.pamant_lucrat = pamant_lucrat;
+        this.timp_lucru = timp_lucru;
+    }
+
+    @Override
+    public String toString() {
+        return "RezultatMotosapa{" +
+                "produs=" + produs +
+                ", curent_consumat=" + curent_consumat +
+                ", volum_ramas=" + volum_ramas +
+                ", functionare_ramasa=" + functionare_ramasa +
+                ", pamant_lucrat=" + pamant_lucrat +
+                ", timp_lucru=" + timp_lucru +
+                '}';
+    }
+}
