6a60f5be52a1341cdb0cff6d23b4c4a23c20ae48
giuliamarin1011 <mm724@student.ugal.ro>
2025-11-20 08:49:41 +0200

    Update RezultatTunsIarba.java

--- RezultatTunsIarba.java
+++ RezultatTunsIarba.java
@@ -1,29 +1,30 @@
 package poo1.aparat;
+   ///neterminata
+
+
+
+
+
+
 
 public class RezultatTunsIarba extends rezultat {
 
     private double iarba_taiata;
     private double timp_folosit;
+    private float[] stare_perii;
+    private float volum_ramas;
+     private double functionare_ramasa;
+                            
+    public RezultatTunsIarba(double produs, double curent_consumat, float[] stare_perii, float volum_ramas, double functionare_ramasa, double iarba_taiata, double timp_folosit) {
 
-    public RezultatTunsIarba(double produs, double curent_consumat,
-                             float[] stare_perii, float volum_ramas,
-                             double functionare_ramasa,
-                             double iarba_taiata, double timp_folosit) {
-
-        super(produs, curent_consumat, stare_perii, volum_ramas, functionare_ramasa);
+        super(produs, curent_consumat);
         this.iarba_taiata = iarba_taiata;
         this.timp_folosit = timp_folosit;
     }
 
-    @Override
-    public String toString() {
-        return "RezultatTunsIarba{" +
-                "produs=" + produs +
-                ", curent_consumat=" + curent_consumat +
-                ", volum_ramas=" + volum_ramas +
-                ", functionare_ramasa=" + functionare_ramasa +
-                ", iarba_taiata=" + iarba_taiata +
-                ", timp_folosit=" + timp_folosit +
-                '}';
-    }
+/// co de facut + ovverride
+
+        
+    
+
 }
