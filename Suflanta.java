870397be76114c4aa1b25354689a6ad51223465a
inhales17 <marineugenia188@gmail.com>
2025-11-20 08:05:36 +0200

    Suflanta.java

--- Suflanta.java
+++ Suflanta.java
@@ -17,8 +17,8 @@ public class Suflanta extends Aparat {
 
     public Suflanta(String denumire, double putere, float viteza_aer,
                     boolean are_aspirare, int nivel_zgomot, float greutate,
-                    int autonomie_baterie, String tip_alimentare, String material_carcasa,
-                    boolean functie_turbo) {
+                    int autonomie_baterie, String tip_alimentare,
+                    String material_carcasa, boolean functie_turbo) {
 
         this.denumire = denumire;
         this.putere = putere;
@@ -32,23 +32,101 @@ public class Suflanta extends Aparat {
         this.functie_turbo = functie_turbo;
     }
 
-    public Suflanta(Suflanta other) {
-        this(other.denumire, other.putere, other.viteza_aer, other.are_aspirare,
-             other.nivel_zgomot, other.greutate, other.autonomie_baterie,
-             other.tip_alimentare, other.material_carcasa, other.functie_turbo);
+    public Suflanta(Suflanta s1) {
+        this(s1.denumire, s1.putere, s1.viteza_aer, s1.are_aspirare,
+                s1.nivel_zgomot, s1.greutate, s1.autonomie_baterie,
+                s1.tip_alimentare, s1.material_carcasa, s1.functie_turbo);
     }
 
-    public RezultatSuflanta sufla(float durata) {
-        double curent = putere * (durata / 60);
-        return new RezultatSuflanta(durata, curent, viteza_aer, durata);
+
+    public String getDenumire() {
+        return denumire;
     }
+
+    public void setDenumire(String denumire) {
+        this.denumire = denumire;
+    }
+
+    public double getPutere() {
+        return putere;
+    }
+
+    public void setPutere(double putere) {
+        this.putere = putere;
+    }
+
+    public float getViteza_aer() {
+        return viteza_aer;
+    }
+
+    public void setViteza_aer(float viteza_aer) {
+        this.viteza_aer = viteza_aer;
+    }
+
+    public boolean isAre_aspirare() {
+        return are_aspirare;
+    }
+
+    public void setAre_aspirare(boolean are_aspirare) {
+        this.are_aspirare = are_aspirare;
+    }
+
+    public int getNivel_zgomot() {
+        return nivel_zgomot;
+    }
+
+    public void setNivel_zgomot(int nivel_zgomot) {
+        this.nivel_zgomot = nivel_zgomot;
+    }
+
+    public float getGreutate() {
+        return greutate;
+    }
+
+    public void setGreutate(float greutate) {
+        this.greutate = greutate;
+    }
+
+    public int getAutonomie_baterie() {
+        return autonomie_baterie;
+    }
+
+    public void setAutonomie_baterie(int autonomie_baterie) {
+        this.autonomie_baterie = autonomie_baterie;
+    }
+
+    public String getTip_alimentare() {
+        return tip_alimentare;
+    }
+
+    public void setTip_alimentare(String tip_alimentare) {
+        this.tip_alimentare = tip_alimentare;
+    }
+
+    public String getMaterial_carcasa() {
+        return material_carcasa;
+    }
+
+    public void setMaterial_carcasa(String material_carcasa) {
+        this.material_carcasa = material_carcasa;
+    }
+
+    public boolean isFunctie_turbo() {
+        return functie_turbo;
+    }
+
+    public void setFunctie_turbo(boolean functie_turbo) {
+        this.functie_turbo = functie_turbo;
+    }
+
+    // ======================== METODE =============================
 
     @Override
     public void schimba_starea(Rezultat r) {
         curent -= r.curent_consumat;
-        System.out.println("[SUFLANTA] " + denumire +
-                " | consum: " + r.curent_consumat +
-                " | curent total: " + curent);
+        System.out.println("SUFLANTA " + denumire +
+                "consum: " + r.curent_consumat +
+                "curent total: " + curent);
     }
 
     @Override
