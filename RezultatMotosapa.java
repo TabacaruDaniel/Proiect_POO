b2da2395567d4772fd876cb98c88d2dd72397bc6
giuliamarin1011 <mm724@student.ugal.ro>
2025-11-20 08:41:04 +0200

    Update RezultatMotosapa.java

--- RezultatMotosapa.java
+++ RezultatMotosapa.java
@@ -5,10 +5,7 @@ public class RezultatMotosapa extends rezultat {
     private double pamant_lucrat;
     private double timp_lucru;
 
-    public RezultatMotosapa(double produs, double curent_consumat,
-                            float[] stare_perii, float volum_ramas,
-                            double functionare_ramasa,
-                            double pamant_lucrat, double timp_lucru) {
+    public RezultatMotosapa(double produs, double curent_consumat,float[] stare_perii, float volum_ramas, double functionare_ramasa, double pamant_lucrat, double timp_lucru) {
 
         super(produs, curent_consumat, stare_perii, volum_ramas, functionare_ramasa);
         this.pamant_lucrat = pamant_lucrat;
