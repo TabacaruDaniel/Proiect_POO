f1cfaa0dd89241b838c8276b745fa67c8bebec40
inhales17 <marineugenia188@gmail.com>
2025-11-20 08:03:15 +0200

    Rezultat.java

--- Rezultat.java
+++ Rezultat.java
@@ -1,19 +1,11 @@
 package ProiectPOO;
 
 public class Rezultat {
-    protected double produs;            
-    protected double curent_consumat;   
+    protected double produs;
+    protected double curent_consumat;
 
     public Rezultat(double produs, double curent_consumat) {
         this.produs = produs;
         this.curent_consumat = curent_consumat;
-    }
-
-    @Override
-    public String toString() {
-        return "Rezultat{" +
-                "produs=" + produs +
-                ", curent_consumat=" + curent_consumat +
-                '}';
     }
 }
