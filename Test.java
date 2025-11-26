3520170e72513e9358ab3d23146113e7e82b4b97
inhales17 <marineugenia188@gmail.com>
2025-11-26 18:47:55 +0200

    Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 package ProiectPOO;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -19,6 +21,7 @@ public class Test {
         AparatSpalarePresiune aparat_2 =
                 new AparatSpalarePresiune("Karcher K5", 1500, 130, 7.5f,
                         20, 20, 6, true, "Electric", 75);
+
         AparatSpalarePresiune aparat_3 = new AparatSpalarePresiune(aparat_2);
         aparat_2.setDenumire("Karcher K5-copie");
 
@@ -93,41 +96,55 @@ public class Test {
             System.out.println(vector_suf[i]);
         }
 
+        Scanner sc = new Scanner(System.in);
+
+        //METODE PARCURGERE VECTOR
+
+        // APARAT SPALARE PRESIUNE
+        System.out.print("\nAre roti (true/false): ");
+        boolean roti = sc.nextBoolean();
+        sc.nextLine();
+
+        System.out.print("Tip motor: ");
+        String motor = sc.nextLine();
+
+        metodaAparate(vector_aparat, roti, motor);
+
+
+// SUFLANTA
+        System.out.print("\nTip alimentare (Cablu/Baterie): ");
+        String alim = sc.nextLine();
+
+        System.out.print("Tip carcasa (Metal/Plastic): ");
+        String car = sc.nextLine(); 
+        metodaSuflante(vector_suf, alim, car);
+    }
+
+    public static void metodaAparate(AparatSpalarePresiune[] v, boolean roti, String motor) {
+        boolean gasit = false;
+
+        for (AparatSpalarePresiune a : v) {
+            if (a.isAre_roti() == roti && a.getTip_motor().equals(motor)) {
+                System.out.println(a);
+                gasit = true;
+            }
+        }
+
+        if (!gasit)
+            System.out.println("Nu exista niciun aparat cu aceste criterii!");
+    }
+
+    public static void metodaSuflante(Suflanta[] v, String alimentare, String carcasa) {
+        boolean gasit = false;
+
+        for (Suflanta s : v) {
+            if (s.getTip_alimentare().equals(alimentare) && s.getMaterial_carcasa().equals(carcasa)) {
+                System.out.println(s);
+                gasit = true;
+            }
+        }
+
+        if (!gasit)
+            System.out.println("Nu exista nicio suflanta cu aceste criterii!");
     }
 }
-
-/*AparatSpalarePresiune[] vector_asp = {
-        new AparatSpalarePresiune("ASP0",1200,110,6.0f,20,20,5,true,"Electric",70),
-        new AparatSpalarePresiune("ASP1",1250,111,6.2f,21,21,5,true,"Electric",71),
-        new AparatSpalarePresiune("ASP2",1300,112,6.4f,22,22,6,false,"Electric",72),
-        new AparatSpalarePresiune("ASP3",1350,113,6.6f,23,23,6,true,"Electric",73),
-        new AparatSpalarePresiune("ASP4",1400,114,6.8f,24,24,7,false,"Electric",74),
-        new AparatSpalarePresiune("ASP5",1450,115,7.0f,25,25,7,true,"Electric",75),
-        new AparatSpalarePresiune("ASP6",1500,116,7.2f,26,26,8,true,"Electric",76),
-        new AparatSpalarePresiune("ASP7",1550,117,7.4f,27,27,8,false,"Electric",77),
-        new AparatSpalarePresiune("ASP8",1600,118,7.6f,28,28,9,true,"Electric",78),
-        new AparatSpalarePresiune("ASP9",1650,119,7.8f,29,29,9,false,"Electric",79)
-}
-for (AparatSpalarePresiune asp : vector_asp) {
-    System.out.println(asp);
-}
-
-Suflanta[] sufl = {
-        new Suflanta("SUF0",800,170,true,75,3.0f,20,"Baterie","Metal",true),
-        new Suflanta("SUF1",820,172,false,76,3.1f,21,"Baterie","Plastic",false),
-        new Suflanta("SUF2",840,174,true,77,3.2f,22,"Cablu","Metal",false),
-        new Suflanta("SUF3",860,176,false,78,3.3f,23,"Cablu","Plastic",true),
-        new Suflanta("SUF4",880,178,true,79,3.4f,24,"Baterie","Metal",false),
-        new Suflanta("SUF5",900,180,false,80,3.5f,25,"Baterie","Plastic",false),
-        new Suflanta("SUF6",920,182,true,81,3.6f,26,"Cablu","Metal",true),
-        new Suflanta("SUF7",940,184,false,82,3.7f,27,"Cablu","Plastic",false),
-        new Suflanta("SUF8",960,186,true,83,3.8f,28,"Baterie","Metal",false),
-        new Suflanta("SUF9",980,188,false,84,3.9f,29,"Baterie","Plastic",true)
-}
-for (Suflanta s : sufl) {
-    System.out.println(s);
-}
-
-
- */
-
