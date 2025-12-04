2f8a2fd8dc815637547425cd5e7c0c55fecd5381
TabacaruDaniel <danieltabacaru5@gmail.com>
2025-12-04 09:47:51 +0200

    Update Rezultat_aspirator.java

--- Rezultat_aspirator.java
+++ Rezultat_aspirator.java
@@ -1,11 +1,16 @@
 package ProiectPOO;
 
+import javax.swing.JOptionPane;
+
 class Rezultat_aspirator extends Rezultat{
     protected float stare_filtru;
     protected float volum_liber;
-    public Rezultat_aspirator(double produs,double curent_consumat,float stare_filtru, float volum_liber){
-        super(produs,curent_consumat);
+    public Rezultat_aspirator(boolean defectiune,double produs,double curent_consumat,float stare_filtru, float volum_liber,float functionare_ramasa){
+        super(defectiune,produs,curent_consumat,functionare_ramasa);
         this.stare_filtru=stare_filtru;
         this.volum_liber=volum_liber;
     }
+public String toString(){
+    return ""+produs+" "+curent_consumat+" "+stare_filtru+" "+volum_liber+" "+functionare_ramasa;
+}
 }
