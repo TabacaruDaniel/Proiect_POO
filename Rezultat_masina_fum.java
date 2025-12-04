45cc9b47ef05c51dfd6f739829419e38267d6ebf
TabacaruDaniel <danieltabacaru5@gmail.com>
2025-12-04 09:48:25 +0200

    Update Rezultat_masina_fum.java

--- Rezultat_masina_fum.java
+++ Rezultat_masina_fum.java
@@ -3,9 +3,8 @@ package ProiectPOO;
 class Rezultat_masina_fum extends Rezultat{
     protected float[] stare_leduri;
     protected float volum_ramas;
-    protected double functionare_ramasa;
-    public Rezultat_masina_fum(double produs,double curent_consumat,float[] stare_leduri, float volum_ramas, float functionare_ramasa){
-        super(produs,curent_consumat);
+    public Rezultat_masina_fum(boolean defectiune,double produs,double curent_consumat,float[] stare_leduri, float volum_ramas, float functionare_ramasa){
+        super(defectiune,produs,curent_consumat,functionare_ramasa);
         this.stare_leduri=stare_leduri;
         this.volum_ramas=volum_ramas;
         this.functionare_ramasa=functionare_ramasa;
