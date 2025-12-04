3db19dc1fe655f0c7bfce6db337442166dd63075
TabacaruDaniel <danieltabacaru5@gmail.com>
2025-12-04 09:47:19 +0200

    Update Rezultat.java

--- Rezultat.java
+++ Rezultat.java
@@ -1,12 +1,14 @@
 package ProiectPOO;
 
 class Rezultat{
+    protected boolean defectiune;
     protected double produs;
     protected double curent_consumat;
-    protected double functionare_ramasa;
-    public Rezultat(double produs,double curent_consumat){
+    protected float functionare_ramasa;
+    public Rezultat(boolean defectiune,double produs,double curent_consumat,float functionare_ramasa){
+        this.defectiune = defectiune;
         this.produs=produs;
         this.curent_consumat=curent_consumat;
-        this.functionare_ramasa-=produs;
+        this.functionare_ramasa=functionare_ramasa;
     }
 }
