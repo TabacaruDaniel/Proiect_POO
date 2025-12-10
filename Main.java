0204a925f08b793788b7c62466ff39527e751776
giuliamarin1011 <mm724@student.ugal.ro>
2025-12-10 20:46:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import javax.swing.JScrollPane;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class Main {
+public class Aparat {
 
 
     public static void main(String[] args) {
